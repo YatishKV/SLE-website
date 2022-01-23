72bfbd6d176f2c3a7631cdf8e6fc4f05ae93a26e
YatishKV <98258884+YatishKV@users.noreply.github.com>
2022-01-23 16:27:11 +0530

    Add files via upload
    deployment config

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from views import views
+import os
 
 app=Flask(__name__)
 app.register_blueprint(views,url_prefix="/")
 
 if __name__ == "__main__":
-    app.run(debug=True,port=8000)
+    port = os.environ.get("PORT", 5000) #heroku sets the PORT environ variable
+    app.run(debug=False, host="0.0.0.0", port=port)
